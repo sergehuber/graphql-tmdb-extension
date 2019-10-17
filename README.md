ce624e6a5f3f1de7615cd81b04dce5b470c49d9b
Serge Huber <shuber@jahia.com>
2019-10-17 07:38:29 +0200

    Refactoring to remove GqlBaseMovie GraphQL type - added call to tmdbService to get Movie actors - added requirements in README documentation.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # graphql-tmdb-gqlMovie-extension
+
+This module contains an extension for the graphql-tmdb-provider module. Make sure you install it first.
--- src/main/java/org/jahia/modules/tmdbmovieextension/graphql/extensions/GqlMovieExtension.java
+++ src/main/java/org/jahia/modules/tmdbmovieextension/graphql/extensions/GqlMovieExtension.java
@@ -1,12 +1,16 @@
 package org.jahia.modules.tmdbmovieextension.graphql.extensions;
 
 import com.uwetrottmann.tmdb2.entities.CastMember;
+import com.uwetrottmann.tmdb2.entities.Movie;
 import graphql.annotations.annotationTypes.GraphQLDescription;
 import graphql.annotations.annotationTypes.GraphQLField;
 import graphql.annotations.annotationTypes.GraphQLTypeExtension;
 import org.jahia.modules.tmdbmovieextension.graphql.api.GqlActor;
+import org.jahia.modules.tmdbprovider.api.TMDBService;
 import org.jahia.modules.tmdbprovider.graphql.api.GqlMovie;
+import org.jahia.osgi.BundleUtils;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -14,19 +18,22 @@ import java.util.List;
 @GraphQLTypeExtension(GqlMovie.class)
 public class GqlMovieExtension {
     private GqlMovie gqlMovie;
+    private TMDBService tmdbService = null;
 
     public GqlMovieExtension(GqlMovie gqlMovie) {
         this.gqlMovie = gqlMovie;
+        this.tmdbService = BundleUtils.getOsgiService(TMDBService.class, null);
     }
 
     @GraphQLField
     @GraphQLDescription("Retrieve the list of actors of a movie")
-    public Collection<GqlActor> getActors() {
+    public Collection<GqlActor> getActors() throws IOException {
         List<GqlActor> gqlActors = new ArrayList<>();
-        if (gqlMovie.getTmdbMovie().credits != null
-                && gqlMovie.getTmdbMovie().credits.cast != null
-                && !gqlMovie.getTmdbMovie().credits.cast.isEmpty()) {
-            for (CastMember castMember : gqlMovie.getTmdbMovie().credits.cast) {
+        Movie tmdbMovie = tmdbService.getMovie(gqlMovie.getTmdbMovie().id);
+        if (tmdbMovie.credits != null
+                && tmdbMovie.credits.cast != null
+                && !tmdbMovie.credits.cast.isEmpty()) {
+            for (CastMember castMember : tmdbMovie.credits.cast) {
                 gqlActors.add(new GqlActor(castMember));
             }
         }
